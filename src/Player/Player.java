ecda76c7b1e45d4129c5652248a22707307a8b9b
Darleanow <enzohugonn@gmail.com>
2024-02-22 20:08:09 +0100

    Enhanced stats

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -7,15 +7,37 @@ public class Player
 {
     boolean is_alive;
     private final IntegerProperty hp;
+    private final IntegerProperty hunger;
+    private final IntegerProperty thirst;
+    private final IntegerProperty tiredness;
+    private final IntegerProperty mentalHealth;
+    private final IntegerProperty strength;
+    private final IntegerProperty agility;
+    private final IntegerProperty stamina;
 
     public final int getHp() { return hp.get(); }
     public final void setHp(int value) { hp.set(value); }
-    public IntegerProperty hpProperty() { return hp; }
+    public IntegerProperty hpProperty() { return this.hp; }
+    public IntegerProperty hungerProperty() { return this.hunger; }
+    public IntegerProperty thirstProperty() { return this.thirst; }
+    public IntegerProperty tirednessProperty() { return this.tiredness; }
+    public IntegerProperty mentalHealthProperty() { return this.mentalHealth; }
+    public IntegerProperty strengthProperty() { return strength; }
+    public IntegerProperty agilityProperty() { return agility; }
+    public IntegerProperty staminaProperty() { return stamina; }
 
     public Player()
     {
         this.is_alive = true;
         this.hp = new SimpleIntegerProperty(100);
+        this.hunger = new SimpleIntegerProperty(100);
+        this.thirst = new SimpleIntegerProperty(100);
+        this.tiredness = new SimpleIntegerProperty(0);
+        this.mentalHealth = new SimpleIntegerProperty(100);
+        this.strength = new SimpleIntegerProperty(10);
+        this.agility  = new SimpleIntegerProperty(10);
+        this.stamina = new SimpleIntegerProperty(10);
+
     }
     public boolean is_alive()
     {
--- src/Widgets/PlayerStatsPanel.java
+++ src/Widgets/PlayerStatsPanel.java
@@ -1,6 +1,7 @@
 package Widgets;
 
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
@@ -29,52 +30,60 @@ public class PlayerStatsPanel {
         statsBox.setPadding(new Insets(15));
         statsBox.setStyle("-fx-background-color: #222; -fx-border-color: #AAA; -fx-border-width: 1;");
 
-        // Font size 32
         Font font32 = this.fontManager.loadFont(32);
         Font font22 = this.fontManager.loadFont(22);
 
         // Titre des statistiques
-        Text title_stats = new Text("Statistiques");
-        title_stats.setFont(font32);
-        title_stats.setTextAlignment(TextAlignment.CENTER);
-        title_stats.setFill(Color.web("#FFC857"));
-        StackPane titleContainer = new StackPane(title_stats);
-        titleContainer.setAlignment(Pos.CENTER);
-        titleContainer.setPadding(new Insets(0, 0, 10, 0));
+        statsBox.getChildren().add(createStatTitle("Statistiques", font32));
 
-        // HP
-        Text hpText = new Text();
-        hpText.textProperty().bind(Bindings.concat("HP: ", player.hpProperty().asString()));
-        hpText = configureText(hpText, font22,Color.web("#E63946"));
-        HBox hpBox = new HBox(hpText);
-        hpBox.setAlignment(Pos.CENTER_LEFT);
+        // Statistiques du joueur
+        statsBox.getChildren().add(createStatLine("HP: ",
+                player.hpProperty(), "#FF6768", font22));
+        statsBox.getChildren().add(createStatLine("Satiete: ",
+                player.hungerProperty(), "#FFD364", font22));
+        statsBox.getChildren().add(createStatLine("Soif: ",
+                player.thirstProperty(), "#68B0AB", font22));
+        statsBox.getChildren().add(createStatLine("Fatigue: ",
+                player.tirednessProperty(), "#596475", font22));
+        statsBox.getChildren().add(createStatLine("Sante Mentale: ",
+                player.mentalHealthProperty(), "#A9DFBF", font22));
+        statsBox.getChildren().add(createStatLine("Force: ",
+                player.strengthProperty(), "#DAA588", font22));
+        statsBox.getChildren().add(createStatLine("Agilite: ",
+                player.agilityProperty(), "#9AD1D4", font22));
+        statsBox.getChildren().add(createStatLine("Endurance: ",
+                player.staminaProperty(), "#C7B198", font22));
 
-        // Faim
-        Text hungerText = new Text("Faim: 0%");
-        hungerText = configureText(hungerText, font22,Color.web("#E67E39"));
-        HBox hungerBox = new HBox(hungerText);
-        hungerBox.setAlignment(Pos.CENTER_LEFT);
 
-        // Energie
-        Text energyText = new Text("Energie: 50");
-        energyText = configureText(energyText, font22,Color.web("#2A9D8F"));
-
-        HBox energyBox = new HBox(energyText);
-        energyBox.setAlignment(Pos.CENTER_LEFT);
-
-        // Ajouter les éléments à la VBox
-        statsBox.getChildren().addAll(titleContainer, hpBox, hungerBox, energyBox/*, build_quests_constructions_pane()*/);
         HBox.setHgrow(statsBox, Priority.ALWAYS);
-
         return statsBox;
     }
 
-    private Text configureText(Text text, Font font, Color color)
-    {
+    private StackPane createStatTitle(String titleText, Font font) {
+        Text title = new Text(titleText);
+        title.setFont(font);
+        title.setFill(Color.web("#FFC857"));
+        title.setTextAlignment(TextAlignment.CENTER);
+        StackPane titleContainer = new StackPane(title);
+        titleContainer.setAlignment(Pos.CENTER);
+        titleContainer.setPadding(new Insets(0, 0, 10, 0));
+        return titleContainer;
+    }
+
+    private HBox createStatLine(String labelText, IntegerProperty property, String color, Font font) {
+        Text label = new Text();
+        label.textProperty().bind(Bindings.concat(labelText, property.asString()));
+        label = configureText(label, font, Color.web(color));
+        HBox lineBox = new HBox(label);
+        lineBox.setAlignment(Pos.CENTER_LEFT);
+        return lineBox;
+    }
+
+    private Text configureText(Text text, Font font, Color color) {
         text.setFont(font);
         text.setFontSmoothingType(FontSmoothingType.LCD);
         text.setFill(color);
-
         return text;
     }
+
 }
